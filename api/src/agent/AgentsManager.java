d998ef25a04ba65e1e4dee74d17158bc806cc136
AntonioHollerman <toniohollerProf@gmail.com>
2025-09-27 15:27:41 -0400

    implement genai api key

--- api/src/agent/AgentsManager.java
+++ api/src/agent/AgentsManager.java
@@ -1,4 +1,15 @@
 package agent;
+import com.google.genai.Client;
+
+import java.util.ResourceBundle;
+
 
 public class AgentsManager {
+    public static Client client;
+    static {
+        var rb = ResourceBundle.getBundle("key");
+        client = Client.builder()
+                .apiKey(rb.getString("apiKey"))
+                .build();
+    }
 }
